13cf718753c781cd58263528ed1629e3bb1c1317
aditgege <aditgege0@gmail.com>
2020-03-09 23:36:41 +0700

    update

---
+++ src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<input v-model="username" />
+		<button @click="login">Login</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Login',
+	data:() => ({
+		username:'',
+	}),
+	methods: {
+		login() {
+			if(this.username) {
+				this.$emit('login', this.username);
+			}
+		}
+	}
+}
+</script>
---
+++ src/components/SelectGame.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<section>
+			<p>Create Game</p>
+			<input v-model="gameName" />
+			<button @click="$emit('create-game', gameName)">Create Game</button>
+		</section>
+		<section>
+			<p>Join Game</p>
+			<input v-model="gameId" placeholder="Enter an existing game" />
+			<button @click="$emit('join-game', gameId)">Join Game</button>	
+		</section>
+	</div>
+</template>
+<script>
+export default {
+	data:() => ({
+		gameName:'',
+		gameId:'',
+	})
+}
+</script>
+<style scoped>
+div {
+	display: flex;
+}
+p {
+  margin: 4px;
+}
+
+section {
+  padding: 16px;
+  background-color: whitesmoke;
+  margin: 16px;
+}
+</style>
